2419b50250298ff615feee7bee722c7f6f428b92
Bhogireddykavya <81571909+Bhogireddykavya@users.noreply.github.com>
2021-03-29 21:27:03 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,11 @@
+class Circle
+public static void main(String a[])
+{
+int radius=10;
+final float=3.14f;
+float area=pi*radius*radius;
+System.out.printin("Area of circle="+area);
+float perimeter=2*pi*radius;
+System.out.printin("Perimeter of circle="+perimeter);
+}
+}
